41c196790447a22457f3a2f3d2be6467dacd6b7e
nexus <38168516+nexus4880@users.noreply.github.com>
2023-02-23 04:46:05 -0700

    removed TestAssaultCube project as it isnt needed as we can use the TestUserMode project. changed driverinterop to take a HANDLE (which is a void*) instead of a PHANDLE (which is a HANDLE*, essentially void**), the only reason it was PHANDLE was so that the value would change in the driverinterop if the value of the handle changed, but the handle shouldnt change at all. removed TestUserMode:memory.h/memory.cpp as we dont need them as the driverinterop does the IOCTL.

--- TestUserMode/driverinterop.hpp
+++ TestUserMode/driverinterop.hpp
@@ -14,27 +14,27 @@ PTRW;
 
 class DriverInterop {
 public:
-	DriverInterop(PHANDLE pHandle, const wchar_t* processName) : m_pDriverHandle(pHandle) {
-		this->m_processId = GetProcessIdByName(processName);
+	DriverInterop(HANDLE handle, const wchar_t* processName) : handle(handle) {
+		this->processId = GetProcessIdByName(processName);
 	}
 
 	~DriverInterop() {
-		if (this->m_pDriverHandle) {
-			CloseHandle(*this->m_pDriverHandle);
+		if (this->handle) {
+			CloseHandle(this->handle);
 		}
 	}
 
 	PTRW get_proc_base_address() {
-		_KERNEL_GET_BASE_ADDRESS_REQUEST getBaseAddressRequest{this->m_processId, 0};
-		DeviceIoControl(*this->m_pDriverHandle, IO_GET_BASE_ADDRESS_REQUEST, &getBaseAddressRequest, sizeof(_KERNEL_GET_BASE_ADDRESS_REQUEST), &getBaseAddressRequest, sizeof(_KERNEL_GET_BASE_ADDRESS_REQUEST), 0, 0);
+		_KERNEL_GET_BASE_ADDRESS_REQUEST getBaseAddressRequest{this->processId, 0};
+		DeviceIoControl(this->handle, IO_GET_BASE_ADDRESS_REQUEST, &getBaseAddressRequest, sizeof(_KERNEL_GET_BASE_ADDRESS_REQUEST), &getBaseAddressRequest, sizeof(_KERNEL_GET_BASE_ADDRESS_REQUEST), 0, 0);
 
 		return getBaseAddressRequest.BaseAddress;
 	}
 
 	template <typename T>
 	bool read(PTRW address, const T& t) {
-		_KERNEL_READ_REQUEST readRequest{this->m_processId, address, sizeof(T), (unsigned char*)&t};
-		if (!DeviceIoControl(*this->m_pDriverHandle, IO_READ_REQUEST, &readRequest, sizeof(_KERNEL_READ_REQUEST), &readRequest, sizeof(_KERNEL_READ_REQUEST), 0, 0)) {
+		_KERNEL_READ_REQUEST readRequest{this->processId, address, sizeof(T), (unsigned char*)&t};
+		if (!DeviceIoControl(this->handle, IO_READ_REQUEST, &readRequest, sizeof(_KERNEL_READ_REQUEST), &readRequest, sizeof(_KERNEL_READ_REQUEST), 0, 0)) {
 			return false;
 		}
 
@@ -63,20 +63,20 @@ public:
 
 	template <typename T>
 	bool write(PTRW address, const T& data) {
-		_KERNEL_WRITE_REQUEST writeRequest{this->m_processId, address, sizeof(T), (unsigned char*)&data};
+		_KERNEL_WRITE_REQUEST writeRequest{this->processId, address, sizeof(T), (unsigned char*)&data};
 		DWORD bytes = 0;
 
-		return DeviceIoControl(*this->m_pDriverHandle, IO_WRITE_REQUEST, &writeRequest, sizeof(_KERNEL_WRITE_REQUEST), nullptr, NULL, &bytes, NULL);
+		return DeviceIoControl(this->handle, IO_WRITE_REQUEST, &writeRequest, sizeof(_KERNEL_WRITE_REQUEST), nullptr, NULL, &bytes, NULL);
 	}
 
 	PTRW GetModuleBaseAddress() {
 		_KERNEL_GET_BASE_MODULE_ADDRESS_REQUEST request{0};
 		DWORD bytes = 0;
-		DeviceIoControl(*this->m_pDriverHandle, IO_GET_MODULE_BASE_REQUEST, &request, sizeof(_KERNEL_GET_BASE_MODULE_ADDRESS_REQUEST), &request, sizeof(_KERNEL_GET_BASE_MODULE_ADDRESS_REQUEST), &bytes, NULL);
+		DeviceIoControl(this->handle, IO_GET_MODULE_BASE_REQUEST, &request, sizeof(_KERNEL_GET_BASE_MODULE_ADDRESS_REQUEST), &request, sizeof(_KERNEL_GET_BASE_MODULE_ADDRESS_REQUEST), &bytes, NULL);
 
 		return request.Value;
 	}
 private:
-	PHANDLE m_pDriverHandle;
-	int m_processId;
+	HANDLE handle;
+	int processId;
 };
--- TestUserMode/main.cpp
+++ TestUserMode/main.cpp
@@ -1,8 +1,67 @@
 #include <iostream>
-#include <Windows.h>
 #include <thread>
+#define AMD64 1
+#include "driverinterop.hpp"
 
-int main()
-{
-	return 0;
+struct Vector3 {
+    float x, y, z;
+};
+
+struct Vector2 {
+    float x, y;
+};
+
+struct Camera3D {
+    Vector3 position;
+    Vector3 target;
+    Vector3 up;
+    float fovy;
+    int projection;
+};
+
+struct rlFPCamera {
+    int ControlsKeys[11];
+    Vector3 MoveSpeed;
+    Vector2 TurnSpeed;
+    bool UseMouse;
+    bool InvertY;
+    float MouseSensitivity;
+    float MinimumViewY;
+    float MaximumViewY;
+    float ViewBobbleFreq;
+    float ViewBobbleMagnatude;
+    float ViewBobbleWaverMagnitude;
+    Vector3 CameraPosition;
+    float PlayerEyesPosition;
+    Vector2 FOV;
+    float TargetDistance;
+    Vector2 ViewAngles;
+    float CurrentBobble;
+    bool Focused;
+    bool AllowFlight;
+    Camera3D ViewCamera;
+    Vector3 Forward;
+    Vector3 Right;
+    double NearPlane;
+    double FarPlane;
+};
+
+int main() {
+    HANDLE handle = {};
+    std::cout << "finding driver";
+    while ((handle = CreateFileW(L"\\\\.\\TestDriver", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)) == INVALID_HANDLE_VALUE) {
+        std::cout << ".";
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+    std::cout << "\nfound driver\n";
+    DriverInterop driver(handle, L"CPPRaylib.exe");
+    rlFPCamera cam{};
+    PTRW base = driver.get_proc_base_address();
+    while (driver.read_chain<rlFPCamera, 3>(base, {0x000264D0, 0x0, 0x8}, cam)) {
+        printf_s("%f, %f, %f\n", cam.CameraPosition.x, cam.CameraPosition.y, cam.CameraPosition.z);
+        std::this_thread::sleep_for(std::chrono::milliseconds(150));
+    }
+
+    return 0;
 }
